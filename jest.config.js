56a935a309e2154a44bd42941786d7aca7bed496
André Gräßel <hello@spaceemotion.net>
2020-04-08 11:50:01 +0200

    chore: Fix jest not generating any coverage data

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,16 @@
+"use strict";
+
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/*.ts", "src/**/*.js"],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+  testEnvironment: "node",
+  preset: "ts-jest",
 };
